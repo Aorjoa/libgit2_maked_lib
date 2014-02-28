5debb6adb6e69711440a8e864c19bc10505dabc3
bhuridech <dekcomgang@hotmail.com>
2014-02-28 14:30:49 +0700

    Init project with compiled files

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/dekcom/Dropbox/repository/usb-data-storage/libgit2
+# Build directory: /Users/dekcom/Dropbox/repository/usb-data-storage/libgit2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(libgit2_clar "libgit2_clar" "-ionline")
